6d2525a61371ce5300c87bfec23961743009bc7a
pavithranp <ptpn94@gmail.com>
2021-01-02 16:31:16 +0100

    conx cony

--- main.cpp
+++ main.cpp
@@ -7,13 +7,14 @@ using namespace Eigen;
 using namespace std;
 
 
-MatrixXf Conv( MatrixXf A,  RowVectorXf V)
+MatrixXf Conv( MatrixXf A,  MatrixXf V)
 {
   //make functor
-  int P = V.cols()/2;
 
-  MatrixXf B = MatrixXf::Zero(A.rows(),A.cols()+(2*P));
-  B.block (0,P,A.rows(),A.cols())=A;
+  int P = V.cols() > V.rows() ? V.cols()/2 : V.rows()/2;
+
+  MatrixXf B = MatrixXf::Zero(A.rows()+(2*P),A.cols()+(2*P));
+  B.block (P,P,A.rows(),A.cols())=A;
   return MatrixXf::NullaryExpr(A.rows(), A.cols(), [&B,&V] (Index i,Index j) {
         return (V.array() * B.block (i,j, V.rows(),V.cols()).array()).sum();
         });
@@ -36,12 +37,13 @@ return kernel;
 int main()
 {
  RowVectorXf kernel = GaussianFilter(2,3);
-
+  VectorXf kernelY= kernel.transpose();
   Image x;
   const char* file_ppm = "../data/tsu1.pgm";
   x.ReadFromDisk(file_ppm);
   cout << x.image_data.topLeftCorner(5,5)<<endl<<endl;
   x.image_data = Conv(x.image_data,kernel);
+  x.image_data = Conv(x.image_data,kernelY);
   cout << x.image_data.topLeftCorner(5,5)<<endl<<endl;
-  x.WriteToDisk("test.pgm");
+  x.WriteToDisk("test.jpg");
 }
