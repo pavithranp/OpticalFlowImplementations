82934a414686f48843cb314231d37afaaf582556
pavithranp <ptpn94@gmail.com>
2021-01-06 23:45:48 +0100

    fix minor bug

--- main.cpp
+++ main.cpp
@@ -1,54 +1,85 @@
-
 #include <Eigen/Dense>
 #include <iostream>
 #include "src/Image.h"
-#include "src/conv_lib.h"
+// #include "src/block_matching.h"
+#include "math.h"
+using namespace Eigen;
+using namespace std;
 
+struct Distance
+{
+     int x;
+     int y;
+};
+Distance search( MatrixXf A,  MatrixXf V)
+{
+  //make functor
 
-MatrixXf padding(MatrixXf input, int padX, int padY) {
-int rl = input.rows(), cl = input.cols();
+  int Py = V.cols()/2 , Px = V.rows()/2;
 
-MatrixXf B(rl+padX, cl+padY);
-B.block(padX/2, padY/2, rl, cl) = input;
-cout << input.row(0).replicate(2, 1) << endl;
-// top-bot row and left-right column fill
-B.block(0, padY/2, padX/2, cl) = input.row(0).replicate(padX/2, 1);
-B.block(rl+padX/2, padY/2, padX/2, cl) = input.row(rl-1).replicate(padX/2, 1);
-B.block(padX/2, 0, rl, padY/2) = input.col(0).replicate(1, padY/2);
-B.block(padX/2, cl+padY/2, rl, padY/2) = input.col(cl-1).replicate(1, padY/2);
-// corner squares
-B.block(0, 0, padX/2, padY/2) =
-                  MatrixXf::Constant(padX/2, padY/2, input.coeff(0, 0));
-B.block(0, cl+padY/2, padX/2, padY/2) =
-                  MatrixXf::Constant(padX/2, padY/2, input.coeff(0, cl-1));
-B.block(rl+padX/2, 0, padX/2, padY/2) =
-                  MatrixXf::Constant(padX/2, padY/2, input.coeff(rl-1, 0));
-B.block(rl+padX/2, cl+padY/2, padX/2, padY/2) =
-                  MatrixXf::Constant(padX/2, padY/2, input.coeff(rl-1, cl-1));
-
-return B;
+  MatrixXf B = MatrixXf::Zero(A.rows()+(2*Px),A.cols()+(2*Py));
+  B.block (Px,Py,A.rows(),A.cols())=A;
+  // cout<<B<<endl<<endl;
+  MatrixXf C =  MatrixXf::NullaryExpr(A.rows(), A.cols(), [&B,&V] (Index i,Index j) {
+                return pow((V.array() - B.block(i,j, V.rows(),V.cols()).array()),2).sum();
+        });
+  Distance D;
+  cout << "cost :" <<C.minCoeff(&D.x, &D.y)<<endl;
+  return D;
+  
 }
 
+RowVectorXf GaussianFilter(float sigma, int kernel_size){
 
+//assert odd kernel size
+RowVectorXf kernel = 
+          RowVectorXf::LinSpaced(kernel_size,-(kernel_size-1)/2,(kernel_size-1)/2);
+
+kernel = (1/(sigma*sqrt(2.0*3.14)))*
+            exp(pow(kernel.array(),2)/(-2.0*sigma*sigma));
+
+kernel = kernel/kernel.sum();
+
+return kernel;
+
+}
 
 int main()
 {
-   MatrixXf A(3,4);
-  A.reshaped() = VectorXf::LinSpaced(12,1,12);
-  cout << A << "\n\n";
-  int N = 5;
-  MatrixXf B = pad1(A,4,2);
-  // B = A(pad{2,5}, pad{2,5});
-  cout << B << "\n\n";
-//  RowVectorXf kernel = GaussianFilter(2.0,3);
-//   VectorXf kernelY = kernel.transpose();
-  // Image x, y;
-  // const char* file_ppm1 = "../data/tsu1.pgm";
-  // x.ReadFromDisk(file_ppm1);
-  // const char* file_ppm2 = "../data/tsu2.pgm";
-  // y.ReadFromDisk(file_ppm2);
+ Matrix3f m;
+  m << 9, 14, 20,
+     11, 18, 20,
+    10, 16, 17;
+  // VectorXf kernelY= kernel.transpose();
+  Image x;
+  
+  const char* file_ppm = "../data/tsu1.pgm";
+  x.ReadFromDisk(file_ppm);
+  MatrixXf K = x.image_data.block(50,50,10,10);
+  MatrixXf B = x.image_data.block(52,53,12,12);
+  // BlockMatching(A,B,3,3);
+    cout<< K<<endl<<endl;
+    cout<< B<<endl<<endl;
+    
+  for(int i=0;i<10;i++)
+  for(int j=0;j<10;j++){
+      m= B.block(i,j,3,3);
+      cout<<m;
+      cout<<i<<" :"<<j;
+      // int i=0,j=9;
+      int s=3+1;
 
-  // cout << x.image_data.topLeftCorner(5, 5) << endl << endl;
-  // cout << x.image_data.topLeftCorner(5, 5) << endl << endl;
-  // x.WriteToDisk("test.jpg");
+      int xmax = min(i+s,9);
+      int ymax = min(j+s,9);
+
+      int xmin = max(0,i-s);
+      int ymin = max(0,j-s);
+      // cout << xmax << endl << ymax << endl << xmin << endl << ymin << endl;
+      // cout << K.block(xmin,ymin,xmax-xmin+1,ymax-ymin) << endl;
+    Distance D = search(K.block(xmin,ymin,xmax-xmin+1,ymax-ymin+1),m);
+ cout<< D.x << " " <<D.y<<endl<<endl;
+
+  }
+        // cout<< D.x << " " <<D.y<<endl<<endl;
+
 }
--- src/block_matching.h
+++ src/block_matching.h
@@ -1,18 +1,47 @@
 
 #include<algorithm>
+using namespace Eigen;
+using namespace std;
+// MatrixXf Cost(MatrixXf A,  MatrixXf B)
+// {
+//   int P = V.cols() > V.rows() ? V.cols()/2 : V.rows()/2;
 
-MatrixXf Cost(MatrixXf A,  MatrixXf B)
-{
-  int P = V.cols() > V.rows() ? V.cols()/2 : V.rows()/2;
+//   MatrixXf B = MatrixXf::Zero(A.rows()+(2*P), A.cols()+(2*P));
 
-  MatrixXf B = MatrixXf::Zero(A.rows()+(2*P), A.cols()+(2*P));
+//   B.block(P, P, A.rows(), A.cols()) = A;
+//   return MatrixXf::NullaryExpr(A.rows(), A.cols(), [&B, &V] (Index i, Index j) {
+//         return (V.array() * B.block (i, j, V.rows(), V.cols()).array()).sum();
+//         });
+// }
 
-  B.block(P, P, A.rows(), A.cols()) = A;
-  return MatrixXf::NullaryExpr(A.rows(), A.cols(), [&B, &V] (Index i, Index j) {
-        return (V.array() * B.block (i, j, V.rows(), V.cols()).array()).sum();
-        });
+MatrixXf padding(MatrixXf image_data, int padX, int padY) {
+int rl = image_data.rows(), cl = image_data.cols();
+
+Eigen::MatrixXf B(rl+padX, cl+padY);
+B.block(padX/2, padY/2, rl, cl) = image_data;
+// cout << input.row(0).replicate(2, 1) << endl;
+// top-bot row and left-right column fill
+B.block(0, padY/2, padX/2, cl) = image_data.row(0).replicate(padX/2, 1);
+B.block(rl+padX/2, padY/2, padX/2, cl) = image_data.row(rl-1).replicate(padX/2, 1);
+B.block(padX/2, 0, rl, padY/2) = image_data.col(0).replicate(1, padY/2);
+B.block(padX/2, cl+padY/2, rl, padY/2) = image_data.col(cl-1).replicate(1, padY/2);
+// corner squares
+B.block(0, 0, padX/2, padY/2) =
+            Eigen::MatrixXf::Constant(padX/2, padY/2, image_data.coeff(0, 0));
+B.block(0, cl+padY/2, padX/2, padY/2) =
+            Eigen::MatrixXf::Constant(padX/2, padY/2, image_data.coeff(0, cl-1));
+B.block(rl+padX/2, 0, padX/2, padY/2) =
+            Eigen::MatrixXf::Constant(padX/2, padY/2, image_data.coeff(rl-1, 0));
+B.block(rl+padX/2, cl+padY/2, padX/2, padY/2) =
+            Eigen::MatrixXf::Constant(padX/2, padY/2, image_data.coeff(rl-1, cl-1));
+
+return B;
 }
-
+struct Distance
+{
+     int x;
+     int y;
+};
 //Algorithm
   // check size of matrix are same
   // loop through each pixel on image 1
@@ -21,46 +50,44 @@ MatrixXf Cost(MatrixXf A,  MatrixXf B)
         // calculate cost of two blocks (sub square sum)
     // find distance with minimum cost
 
-
-MatrixXf BlockMatching(MatrixXf A, MatrixXf B, int sx, int sy, int block = 3) {
-    MatrixXf Apad = padding(A, block, block);
-    MatrixXf Bpad = padding(B, block, block);
-
-    MatrixXf::NullaryExpr(A.rows(), A.cols(), [&Apad, &Bpad](Index i, Index j) {
-        sx += (block/2);
-        sy += (block/2);
-
-        int xmax = std::min(i+sx, 10);
-        int ymax = std::min(j+sy, 10);
-        int xmin = std::max(0, j-sx);
-        int ymin = std::max(0, j-sy);
-
-        // cout<<xmax<<endl<<ymax<<endl<<xmin<<endl<<ymin<<endl;
-        // cout << K.block(xmin,ymin,xmax-xmin+1,ymax-ymin+1) << endl;
-        Distance D =
-            SearchSpace(B.block(xmin, ymin, xmax-xmin+1, ymax-ymin+1),
-                        A.block(i, j, block, block));
-        });
-}
-
 Distance SearchSpace(MatrixXf A,  MatrixXf V)
 {
-  // make functor
+  // check pad
   int Py = V.cols()/2 , Px = V.rows()/2;
 
   MatrixXf B = MatrixXf::Zero(A.rows()+(2*Px), A.cols() + (2*Py));
   B.block(Px, Py, A.rows(), A.cols()) = A;
-  // cout<<B<<endl<<endl;
-  MatrixXf C = MatrixXf::NullaryExpr(A.rows(), A.cols(), [&B,&V] (Index i,Index j) {
-        return pow((V.array() - B.block(i,j, V.rows(),V.cols()).array()),2).sum();
+  cout<<B<<endl<<endl;
+  MatrixXf C = MatrixXf::NullaryExpr(A.rows(), A.cols(), [&B, &V] (Index i, Index j) {
+        return pow((V.array() - B.block(i, j, V.rows(),V.cols()).array()),2).sum();
         });
   Distance D;
   cout << "cost :" <<C.minCoeff(&D.x, &D.y) << endl;
   return D;
 }
 
-struct Distance
-{
-     int x;
-     int y;
-};
+void BlockMatching(MatrixXf A, MatrixXf B, int sx, int sy, int block = 3) {
+    // MatrixXf Apad = padding(A, block/2, block/2);
+    // MatrixXf Bpad = padding(B, block/2, block/2);
+    cout<< "print";
+    MatrixXf::NullaryExpr(A.rows(), A.cols(), [&A, &B, &sx,&sy,&block](Index i, Index j) {
+        // sx += ;
+        // sy += (block/2);
+cout<< "print";
+        int xmax = std::min((int)i+sx+(block/2), 10);
+        int ymax = std::min((int)j+sy+(block/2), 10);
+        int xmin = std::max(0, (int)j-sx-(block/2));
+        int ymin = std::max(0, (int)j-sy-(block/2));
+
+        // cout<<xmax<<endl<<ymax<<endl<<xmin<<endl<<ymin<<endl;
+        // cout << K.block(xmin,ymin,xmax-xmin+1,ymax-ymin+1) << endl;
+        Distance D =
+            SearchSpace(B.block(xmin, ymin, xmax-xmin+1, ymax-ymin+1),
+                        A.block(i, j, block, block));
+        cout << D.x << " " << D.y << endl;
+        });
+    
+}
+
+
+
