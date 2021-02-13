4f28ad260b852efadbaadfb6c7dddd79a1950918
pavithranp <ptpn94@gmail.com>
2021-02-13 12:14:25 +0100

    update readme

--- README.md
+++ README.md
@@ -9,3 +9,16 @@ A simple library with Implementations of various OpticalFlow methods using Class
 ### Depends on:
 * stb_image headers
 * Eigen3 Library
+
+### sample outputs -There is a slight inward movement in the simulated images
+
+| Frame1    | Frame2       |
+| -------------- | -------------- |
+![yos1](data/yos1.jpg) |   ![yos2](data/yos2.jpg)
+
+#### Block Matching with windows size 7
+![block matching](data/bm.jpg)
+
+#### Horn and Schunk - single iteration with alpha 100
+![yos1](data/hs.jpg)
+
--- main.cpp
+++ main.cpp
@@ -2,8 +2,8 @@
 #include <Eigen/Dense>
 #include <iostream>
 #include <math.h>
-// #include "block_matching/block_matching.h"
-#include "HornSchunck/HornSchunck.h"
+#include "block_matching/block_matching.h"
+// #include "HornSchunck/HornSchunck.h"
 #include "util/Image.h"
 
 
@@ -14,8 +14,8 @@ int main() {
   // VectorXf kernelY= kernel.transpose();
   Image x, y, z;
 
-  const char* file_ppm1 = "../data/yos1.pgm";
-  const char* file_ppm2 = "../data/yos2.pgm";
+  const char* file_ppm1 = "../data/yos2.pgm";
+  const char* file_ppm2 = "../data/yos1.pgm";
   x.ReadFromDisk(file_ppm1);
   y.ReadFromDisk(file_ppm2);
   // RowVectorXf v = RowVectorXf::LinSpaced(20, 0, 19);
@@ -27,9 +27,9 @@ int main() {
   int block_size = 5;
   // MatrixXf m;
   int s = 1;
-  // z = BlockMatching(x, y, s, block_size);
-  z = HornSchunck(x, y, s, block_size);
-  z.Write3ToDisk("test2.jpg");
+  z = BlockMatching(x, y, s, block_size);
+  // z = HornSchunck(x, y, s, block_size);
+  z.Write3ToDisk("test.jpg");
   // y.Write3ToDisk("test.jpg");
   // cout << "done";
 }
